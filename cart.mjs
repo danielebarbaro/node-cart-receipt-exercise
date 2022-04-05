5eece4504ed0282329ecb42c780c27c7237b46a1
Daniele Quasimodo <ICTS21-23.241@studenti.local>
2022-04-05 17:01:41 +0200

    Aggiunta data

--- cart.mjs
+++ cart.mjs
@@ -1,14 +1,49 @@
 import {carts, products, promoCode, users} from "./dataset.mjs";
 import * as core from "./core-cart.mjs";
 
-console.log('Promo', promoCode);
-console.log('Products', products);
-console.log('Users', users);
-console.log('Cart', carts);
+import * as fs from "fs";
+import * as os from "os";
 
-console.log('Discount', core.discountedPrice(100, 0.2));
 
-for (let cartRow of carts) {
+// Costanti
+const machine = os.userInfo();
+const myUsername = machine.username.toUpperCase(); // Nessuno di questi valori dovrebbe poter cambiare a runtime quindi non serve ricalcolarli ogni volta
+const myPid = String(process.pid);
 
-    // genera ricevuta
+
+
+// Funzioni
+
+const formattedDate = function() {
+    let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+    let d = new Date();
+
+    let dayName = days[d.getDay()];
+    let monthName = months[d.getMonth()];
+    let dayNumberPadded = ("00" + d.getDay().toString()).slice(-2);
+    let year = d.getFullYear();
+
+    return `${dayName} ${monthName} ${dayNumberPadded} ${year}`
 }
+
+const header = () => `${myUsername} Cart - ${myPid}\n${formattedDate()}`;
+
+const scontrino = function(cart) {
+
+
+
+}
+
+
+
+// Main
+
+console.log(header());
+
+//for (let cart of carts) {
+//
+//
+//
+//}
