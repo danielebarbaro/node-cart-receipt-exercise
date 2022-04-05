302e9cd97a88d08e844456d5264f3731accbbd5a
simone.oliva <simone.oliva.work@gmail.com>
2022-04-05 16:21:19 +0200

    bozza

--- cart.mjs
+++ cart.mjs
@@ -1,14 +1,31 @@
 import {carts, products, promoCode, users} from "./dataset.mjs";
 import * as core from "./core-cart.mjs";
 
-console.log('Promo', promoCode);
+/*console.log('Promo', promoCode);
 console.log('Products', products);
 console.log('Users', users);
 console.log('Cart', carts);
 
-console.log('Discount', core.discountedPrice(100, 0.2));
+console.log('Discount', core.discountedPrice(100, 0.2));*/
 
 for (let cartRow of carts) {
 
     // genera ricevuta
+    const { user: uuid, products} = cartRow;
+    const user = core.getUser(uuid);
+    core.printDelimiter(`+`, '-', 50);
+    console.log(`${core.printShopName()}`);
+    console.log(`${core.getDate()}`);
+    core.printDelimiter(`*`, '-', 50);
+    for (let item in cartRow.products){
+        console.log(`   ${item.ean}    ${item.name}\t\t${item.price}`);
+    }
+    core.printDelimiter(`*`, '-', 50);
+    console.log(`   Totale:\t\t\t${core.sumTotale(uuid)}`);
+    core.printDelimiter(`*`, '-', 50);
+    console.log(`\n`);
+    core.printDelimiter(`**`, '-', 50);
+    console.log(`   ${user.firstName} ${user.lastName} ha un credito residuo di ${user.wallet.toFixed(2)}`);
+    core.printDelimiter(`**`, '-', 50);
+
 }
--- core-cart.mjs
+++ core-cart.mjs
@@ -5,6 +5,47 @@ import * as os from "os";
 
 const discountedPrice = (price, rate = 0.10) => (price * (1 - rate)).toFixed(2);
 
+const printShopName = () =>  `${os.userInfo().username.toUpperCase()} - Cart ${process.pid}`;
+
+const printDelimiter = (startFinisc, innerSymbol, n) => {
+    let delimiter = "";
+    delimiter += startFinisc;
+    delimiter += " ";
+    for (let index = 0; index < n; index++){
+        delimiter += innerSymbol;
+    }
+    delimiter += " ";
+    delimiter += startFinisc;
+    console.log(delimiter);
+}
+
+const sumTotale = (uuid) => {
+    products.reduce((sum, item) =>{
+        if(item.uuid === uuid){
+            sum += item.price;
+        }        
+    })
+}
+
+const getDate = () => {
+    let options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' };
+    let date = new Date();
+    return date.toLocaleString(`it-IT`, options);
+}
+
+const formatList = (productList) => {
+
+} 
+const getUser = (uuid) => users.find(user => user.uuid === uuid);
+
+const getProduct = (ean) => products.find(product => product.ean === ean);
+
 export {
     discountedPrice,
+    printShopName,
+    getUser,
+    getProduct,
+    printDelimiter,
+    getDate,
+    sumTotale
 };
