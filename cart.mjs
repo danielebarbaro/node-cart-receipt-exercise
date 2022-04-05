2df22be15243be83d59680dc6d44833a6bdb28fe
Rdx <100215987+Rdxv@users.noreply.github.com>
2022-04-05 16:02:00 +0200

    allineato con la base, da iniziare

--- cart.mjs
+++ cart.mjs
@@ -1,14 +1,31 @@
 import {carts, products, promoCode, users} from "./dataset.mjs";
 import * as core from "./core-cart.mjs";
-
+/*
 console.log('Promo', promoCode);
 console.log('Products', products);
 console.log('Users', users);
 console.log('Cart', carts);
-
-console.log('Discount', core.discountedPrice(100, 0.2));
+*/
+//console.log('SHOP NAME', core.printShopName());
 
 for (let cartRow of carts) {
+    const {user: cartUserUUID, products} = cartRow;
+    const user = core.getUser(uuid);
+    const {uuid, firstName, lastName, wallet, isTeacher, promo} = user;
+
+    //console.log('RIGA DEL CARRELLO DA STAMPARE', cartRow)
+    //console.log('UUID', uuid)
+    //console.log('Prodotti', products)
+    //console.log('User complete', user);
+
+ // trovare uno user dato il carrello
+
+
+ // capire se l' utente può comprare
+
+ // capire se l' utente ha delle promo
+
+
 
     // genera ricevuta
 }
--- core-cart.mjs
+++ core-cart.mjs
@@ -5,6 +5,50 @@ import * as os from "os";
 
 const discountedPrice = (price, rate = 0.10) => (price * (1 - rate)).toFixed(2);
 
+const helloworld = (name => `Hello ${name}`);
+
+const printShopName = () => {
+    const { username } = os.userInfo();     // ottiene il nome utente della macchina
+    return `${machineName.toUpperCase()} = cart ${pid}`;
+} 
+ 
+ const getUser = (uuid) => users.find(user => user.uuid === uuid);
+ 
+ const formatProductName = (product) => product;
+ 
+ const filterType = (products, type) => product;
+
+ const sumCartItem = (products, type) => (products);
+
+ const formatProductList = (products) => (products);
+ 
+ const printReceipt = (content, filename) => ``;
+
+ const createDelimiter = (openClose, symbol, times);
+
+ const getProductByCart = (products) => products;
+
+ const getUserDiscount = () => 0;
+
+ const receiptFileName = (uuid, date) => ``;
+    
+
+    
+    
+
+
 export {
     discountedPrice,
+    helloworld,
+    printShopName,
+    getUser,
+    formatProductName,
+    filterType,
+    sumCartItem,
+    formatProductList,
+    printReceipt,
+    createDelimiter,
+    getProductByCart,
+    getUserDiscount,
+    receiptFileName,
 };
