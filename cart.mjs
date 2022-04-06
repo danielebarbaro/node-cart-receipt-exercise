efa25578ee79c4ec830d244071d0ca4b6226c2c7
simone.oliva <simone.oliva.work@gmail.com>
2022-04-06 09:21:09 +0200

    attempt 1 write to file

--- cart.mjs
+++ cart.mjs
@@ -7,23 +7,28 @@ console.log('Users', users);
 console.log('Cart', carts);
 
 console.log('Discount', core.discountedPrice(100, 0.2));*/
-
+let counterCart = 0;
 for (let cartRow of carts) {
 
+    let receiptText = ``;
     // genera ricevuta
-    console.log(`\n\n`);
+    //console.log(`\n\n`);
     const { user: uuid, products} = cartRow;
     const user = core.getUser(uuid);
-    console.log(core.printDelimiter(`+`, '-', 50));
-    console.log(`${core.printShopName()}`);
-    console.log(`${core.getDate()}`);
-    console.log(core.printDelimiter(`*`, '-', 50));
-    core.formatList(cartRow.products);
-    console.log(core.printDelimiter(`*`, '-', 50));
-    console.log(`   Totale:\t\t\t\t${core.sumTotale(cartRow.products, user)}`);
-    console.log(core.printDelimiter(`*`, '-', 50));
-    let spesa = core.printPromo(cartRow.products ,user);
-    console.log(core.printDelimiter(`**`, '-', 50));
-    console.log(`   ${user.firstName} ${user.lastName} ha un credito residuo di ${((user.wallet) - spesa).toFixed(2)}`);
-    console.log(core.printDelimiter(`**`, '-', 50));
+    receiptText.concat(core.printDelimiter(`+`, '-', 50));
+    receiptText.concat( `\n`, `${core.printShopName()}`);
+    receiptText.concat( `\n`, `${core.getDate()}`);
+    receiptText.concat( `\n`, core.printDelimiter(`*`, '-', 50));
+    receiptText.concat( `\n`, core.formatList(cartRow.products));
+    receiptText.concat( `\n`, core.printDelimiter(`*`, '-', 50));
+    receiptText.concat( `\n`, `   Totale:\t\t\t\t${core.sumTotale(cartRow.products, user)}`);
+    receiptText.concat( `\n`, core.printDelimiter(`*`, '-', 50));
+    receiptText.concat(core.printPromo(cartRow.products ,user));
+    receiptText.concat( `\n`, core.printDelimiter(`**`, '-', 50));
+    receiptText.concat( `\n`, `   ${user.firstName} ${user.lastName} ha un credito residuo di ${((user.wallet) - core.totaleScontato(user, core.sumTotale(cartRow.products , user))).toFixed(2)}`);
+    receiptText.concat( `\n`, core.printDelimiter(`**`, '-', 50));
+
+    console.log(receiptText);
+    core.printReceipt(receiptText, counterCart);
+    counterCart++;
 }
--- core-cart.mjs
+++ core-cart.mjs
@@ -3,7 +3,7 @@ import {carts, products, promoCode, users} from "./dataset.mjs";
 import * as fs from "fs";
 import * as os from "os";
 
-const discountedPrice = (price, rate = 0.10) => (price * (1 - rate)).toFixed(2);
+const discountedPrice = (price, rate) => (price * (1 - rate)).toFixed(2);
 
 const printShopName = () =>  `${os.userInfo().username.toUpperCase()} - Cart ${process.pid}`;
 
@@ -12,16 +12,15 @@ const printDelimiter = (startFinisc, innerSymbol, n) => `${startFinisc} ${innerS
 const format = (word) => word.toLowerCase().charAt(0).toUpperCase() + word.toLowerCase().slice(1);
 
 const formatList = (list) => {
-    let prodotto;
     let counter = 0;
     for ( let codice of list ) {
-        prodotto = products.find(product => product.ean === codice);
+        let prodotto = products.find(product => product.ean === codice);
         let formattedName = ``;
         let multiName = prodotto.name.split(' ');
         if (multiName.length > 1) {
             for (let word of multiName){
                 if (counter === 0) {
-                    formattedName = formattedName.concat(`${format(word)}`);
+                    formattedName += `${format(word)}`;
                     counter++;
                 }else {
                     formattedName = formattedName.concat(` `,`${format(word)}`);
@@ -30,16 +29,15 @@ const formatList = (list) => {
         }else {
             formattedName = `${format(prodotto.name)}`;
         }
-        console.log(`   [${codice}]\t${formattedName}${((prodotto.name.split(` `).length > 1 ) ? `\t\t` : `\t\t\t`)}${prodotto.price.toFixed(2)}`); ;
+        return `   [${codice}]\t${formattedName}${((prodotto.name.split(` `).length > 1 ) ? `\t\t` : `\t\t\t`)}${prodotto.price.toFixed(2)}`;
     }
 }
 
-const sumTotale = (lista, currentUser) => {
+const sumTotale = (lista) => {
     let totale = 0;
-    let prodotto;
     if (lista != null){
         for (let codice of lista) {
-            prodotto = products.find(product => product.ean === codice);
+            let prodotto = products.find(product => product.ean === codice);
             totale += prodotto.price;
         }
     }
@@ -48,23 +46,26 @@ const sumTotale = (lista, currentUser) => {
 }
 
 const printPromo = (lista, currentUser) => {
-    let totale = 0;
-    let prodotto;
-    if (lista != null){
-        for (let codice of lista) {
-            prodotto = products.find(product => product.ean === codice);
-            totale += prodotto.price;
-        }
-    }
+    let result = ``;
+    let totale = sumTotale(lista)
     if (currentUser.promo !== `` && currentUser.promo != undefined) {
-        let rate = promoCode.find(code => code.name === currentUser.promo);
-        totale = totale - (totale * rate.percentage);
-        console.log(`   ${rate.name}\t\t\t\t${(rate.percentage*100).toFixed(2)}%`);
-        console.log(`   Totale scontato:\t\t\t${totale.toFixed(2)}`);
+        let sconto = promoCode.find(code => code.name === currentUser.promo);
+        totale = discountedPrice(totale, sconto.percentage);
+        result = `   ${sconto.name}:\t\t\t\t${(sconto.percentage*100).toFixed(2)}%\n   Totale scontato:\t\t\t${totale}`;
     }else {
         console.log(`\n`);
     }
-   return totale;
+    return result;
+}
+
+const totaleScontato = (currentUser, totaleNonScontato) => {
+    
+    if (currentUser.promo !== `` && currentUser.promo != undefined) {
+        let sconto = promoCode.find(code => code.name === currentUser.promo);
+        return discountedPrice(totaleNonScontato, sconto.percentage);
+    }else {
+        return 0;
+    }
 }
 
 const getDate = () => {
@@ -75,6 +76,15 @@ const getDate = () => {
 
 const getUser = (uuid) => users.find(user => user.uuid === uuid);
 
+const printReceipt = (receiptText, counterCart) => {
+    fs.writeFile(`./receipts/receipt${counterCart}.txt`, receiptText, err => {
+        if (err) {
+          console.error(err)
+          return
+        }
+      });
+}
+
 export {
     discountedPrice,
     printShopName,
@@ -83,5 +93,7 @@ export {
     getDate,
     sumTotale,
     formatList,
-    printPromo
+    printPromo,
+    printReceipt,
+    totaleScontato
 };
