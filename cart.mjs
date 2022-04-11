48a1bf314e815475a893f713e7359e7a3aaea908
Davide <99410035+SkyronVarn@users.noreply.github.com>
2022-04-12 01:04:50 +0200

    cart-receipt-exercise-Sabetta
    non funziona totalmente, non calcola il totale scontato perché non trova la percentuale sconto dallo User
    non penso di riuscire a fare di meglio con la mia attuale conoscenza di JS

--- cart.mjs
+++ cart.mjs
@@ -1,14 +1,74 @@
 import {carts, products, promoCode, users} from "./dataset.mjs";
 import * as core from "./core-cart.mjs";
+import { serialize } from "v8";
 
-console.log('Promo', promoCode);
-console.log('Products', products);
-console.log('Users', users);
-console.log('Cart', carts);
 
-console.log('Discount', core.discountedPrice(100, 0.2));
+for(let user of users){
+
+console.log(core.createDelimiter())
+console.log("  Processing " + core.formatProductName(user.firstName) + " " + core.formatProductName(user.lastName) + " cart:");
+console.log(core.createDelimiter2())
 
 for (let cartRow of carts) {
-
-    // genera ricevuta
+    
+   
+   if (user.uuid == cartRow.user)
+   cartRow.products.forEach(product => console.log("  ["
+    + core.getProduct(product).ean + "]   "
+    + core.formatProductName(core.getProduct(product).name) 
+    + "  \t"+core.getProduct(product).price.toFixed(2) + "€"))
+   
 }
+
+console.log(core.createDelimiter2())
+
+var somma = 0;
+var totscont = 0;
+for (let cartRow of carts) {
+   if (user.uuid == cartRow.user)
+   {
+      cartRow.products.forEach(product =>
+      somma += core.getProduct(product).price)
+      if(user.promo === undefined || user.promo.length === 0 )
+         somma += 0;
+      else{
+      for(let promo of promoCode)
+         totscont = core.discountedPrice(somma,core.getUserDiscount(user))
+      }
+   }
+}
+
+console.log("  Totale non scontato:\t" + somma.toFixed(2));
+console.log("  Totale scontato:\t" + totscont);
+
+console.log("\n\n")
+
+
+var totcred = core.calculateWallet(user.wallet,somma);
+
+console.log(core.createDelimiter3())
+console.log(   "  " + core.formatProductName(user.firstName) 
+       + " " + core.formatProductName(user.lastName) 
+       + "'s remaining credit: " + core.assicuredWallet(totcred))
+console.log(core.createDelimiter3())
+
+console.log("\n\n");
+
+}
+/*
+    + -------------------------------------------------- +
+    NOMEMACCHINA Cart - 43874
+    Mon Apr 04 2022
+    * -------------------------------------------------- *
+       [120193]    Alpi 		    22.10
+       [812302]    Firenze 		    9.99
+       [912301]    Roma 		    9.99
+       [912303]    Pisa 		    9.99
+    * -------------------------------------------------- *
+       Totale: 			  52.07
+    + -------------------------------------------------- +
+    
+    ** -------------------------------------------------- **
+       Winston Wolf ha un credito residuo di 15.25
+    ** -------------------------------------------------- **
+*/
--- core-cart.mjs
+++ core-cart.mjs
@@ -3,8 +3,72 @@ import {carts, products, promoCode, users} from "./dataset.mjs";
 import * as fs from "fs";
 import * as os from "os";
 
-const discountedPrice = (price, rate = 0.10) => (price * (1 - rate)).toFixed(2);
+const discountedPrice = (price, rate = 0.10 ) => (price * (1 - rate)).toFixed(2);
+
+//(price, rate ) => (price * (1 - rate))
+
+const getUser = (uuid) => users.find(user => user.uuid === uuid);
+
+const getProduct = (ean) => products.find(product => product.ean === ean);
+
+const formatProductName = (product) => product.toLowerCase().charAt(0).toUpperCase() + product.toLowerCase().slice(1);
+
+const filterType = (products,type) => {for( pr of products) {pr.type === type ? true : false}};
+
+
+const sumCartItem = (user) => {
+   var somma = 0;
+for (let cartRow of carts) {
+   if (user.uuid == cartRow.user)
+   {
+      cartRow.products.forEach(product =>
+      somma += core.getProduct(product).price)
+   }
+}
+return somma;
+}
+
+const createDelimiter = (openClose, symbol, times) => '+ -------------------------------------------------- +';
+const createDelimiter2 = (openClose, symbol, times) => '* -------------------------------------------------- *';
+const createDelimiter3 = (openClose, symbol, times) => '** -------------------------------------------------- **';
+
+const calculateWallet = (wallet, totale) => wallet - totale;
+const assicuredWallet = (tot) => tot >= 0 ? tot.toFixed(2) + "€":'Empty' ;
+
+const getUserDiscount = (user) => user.promo === promoCode.name ? promoCode.percentage : null ;
+
+
+
 
 export {
     discountedPrice,
+    getUser,
+    getProduct,
+    formatProductName,
+    filterType,
+    sumCartItem,
+    createDelimiter,
+    createDelimiter2,
+    createDelimiter3,
+    calculateWallet,
+    getUserDiscount,
+    assicuredWallet,
 };
+
+/*
+    + -------------------------------------------------- +
+    NOMEMACCHINA Cart - 43874
+    Mon Apr 04 2022
+    * -------------------------------------------------- *
+       [120193]    Alpi 		    22.10
+       [812302]    Firenze 		    9.99
+       [912301]    Roma 		    9.99
+       [912303]    Pisa 		    9.99
+    * -------------------------------------------------- *
+       Totale: 			  52.07
+    + -------------------------------------------------- +
+    
+    ** -------------------------------------------------- **
+       Winston Wolf ha un credito residuo di 15.25
+    ** -------------------------------------------------- **
+*/
