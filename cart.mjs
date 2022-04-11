f5d1e570d859a9438e4a4cc597b48d9f35f71164
D4n13l3Q <daniele.quasimodo@edu.itspiemonte.it>
2022-04-11 11:05:02 +0200

    Spostate funzioni da cart.mjs a core-cart.mjs

--- cart.mjs
+++ cart.mjs
@@ -14,50 +14,28 @@ const myPid = String(process.pid);
 
 // Funzioni
 
-const safeSum = (a, b) => ((a * 100) + (b * 100)) / 100; // Apparentemente javascript e i decimali non vanno d'accordo.
-
-const safeSubtraction = (a, b) => ((a * 100) - (b * 100)) / 100;
-
-const capitalize = (str) => `${str[0].toUpperCase()}${str.substring(1).toLowerCase()}`;
-
-const header = () => `${myUsername} Cart - ${myPid}\n${new Date().toDateString()}`;
-
-const frame = (sideStr, centralChar, width) => `${sideStr} ${centralChar.repeat( width - ((sideStr.length * 2) + 2) )} ${sideStr}`;
-
-const alignLeftRight = (strLeft, strRight, width) => strLeft + (" ".repeat(width-strLeft.length) + strRight).slice(-(width-strLeft.length));
-
-const formatProduct = (product) => alignLeftRight(`[${product.ean}]    ${capitalize(product.name)}`,`${product.price.toFixed(2)}    `,50);
-
-const getProductByEan = (ean) => products.find(product => product.ean === ean);
-
-const getUserByUuid = (uuid) => users.find(user => user.uuid === uuid);
-
-const getPromoByName = (name) => promoCodes.find(promoCode => promoCode.name === name);
-
-const getDiscountedPrice = (price, discount) => safeSubtraction(price, price * discount);
-
 const checkout = function(cart) {
     
     // Elaboro carrello
 
-    let cartUser = getUserByUuid(cart.user);
+    let cartUser = core.getUserByUuid(cart.user);
     
-    console.log(`Processing ${capitalize(cartUser.firstName)} ${capitalize(cartUser.lastName)}'s cart... `); // Facciamo sapere all'utente che stiamo facendo.
+    console.log(`Processing ${core.capitalize(cartUser.firstName)} ${core.capitalize(cartUser.lastName)}'s cart... `); // Facciamo sapere all'utente che stiamo facendo.
     
     if (cart.products.length === 0)
         throw {name : "CartEmptyError", message : "This cart is empty!"};
     
-    let cartProducts = cart.products.map(ean => getProductByEan(ean));
+    let cartProducts = cart.products.map(ean => core.getProductByEan(ean));
     
-    let cartDiscount = (cartUser.promo === undefined || cartUser.promo.length === 0) ? 0 : getPromoByName(cartUser.promo).percentage; // Se promo è undefined o stringa vuota imposto lo sconto a zero, altrimenti imposto lo sconto secondo la promozione.
+    let cartDiscount = (cartUser.promo === undefined || cartUser.promo.length === 0) ? 0 : core.getPromoByName(cartUser.promo).percentage; // Se promo è undefined o stringa vuota imposto lo sconto a zero, altrimenti imposto lo sconto secondo la promozione.
     
-    let totalUndiscounted = cartProducts.reduce( (subtotal, product) => safeSum(subtotal, product.price), 0); // Lo zero alla fine è il valore iniziale di subtotal.
-    let totalDiscounted = cartProducts.reduce( (subtotal, product) => safeSum(subtotal, getDiscountedPrice(product.price, cartDiscount)), 0); // Lo zero alla fine è il valore iniziale per subtotal.
+    let totalUndiscounted = cartProducts.reduce( (subtotal, product) => core.safeSum(subtotal, product.price), 0); // Lo zero alla fine è il valore iniziale di subtotal.
+    let totalDiscounted = cartProducts.reduce( (subtotal, product) => core.safeSum(subtotal, core.discountedPrice(product.price, cartDiscount)), 0); // Lo zero alla fine è il valore iniziale per subtotal.
     
-    if (safeSubtraction(cartUser.wallet, totalDiscounted) < 0)
+    if (core.safeSubtraction(cartUser.wallet, totalDiscounted) < 0)
         throw {name : "BalanceTooLowError", message : "This user's balance is too low to purchase the products in their cart!"};
     
-    cartUser.wallet = safeSubtraction(cartUser.wallet, totalDiscounted);
+    cartUser.wallet = core.safeSubtraction(cartUser.wallet, totalDiscounted);
     
     console.log("Done!"); // La parte dove qualcosa poteva andare storta dovrebbe essere passata.
     
@@ -67,38 +45,38 @@ const checkout = function(cart) {
         
     let receipt = []; // Per comodità gestiamo la ricevuta come un array di stringhe e solo all'ultimo le uniamo insieme (così evitiamo problemi con concat ed è anche tutto più leggibile).
 
-    receipt.push(frame("+", "-", 54));
+    receipt.push(core.frame("+", "-", 54));
     
     receipt.push(`${myUsername} Cart - ${myPid}`);
     receipt.push(`${new Date().toDateString()}`);
     
-    receipt.push(frame("*", "-", 54));
+    receipt.push(core.frame("*", "-", 54));
     
-    cartProducts.forEach(product => receipt.push(alignLeftRight(`   [${product.ean}]    ${capitalize(product.name)}`, `${product.price.toFixed(2)}    `, 54)));
+    cartProducts.forEach(product => receipt.push(core.alignLeftRight(`   [${product.ean}]    ${core.capitalize(product.name)}`, `${product.price.toFixed(2)}    `, 54)));
     
-    receipt.push(frame("*", "-", 54));
+    receipt.push(core.frame("*", "-", 54));
     
-    receipt.push(alignLeftRight("   Totale:", `${totalUndiscounted.toFixed(2)}    `, 54));
+    receipt.push(core.alignLeftRight("   Totale:", `${totalUndiscounted.toFixed(2)}    `, 54));
     
-    receipt.push(frame("+", "-", 54));
+    receipt.push(core.frame("+", "-", 54));
     
     if (cartDiscount > 0) {
         
-        receipt.push(alignLeftRight("   Sconto:", `${safeSubtraction(totalUndiscounted,totalDiscounted).toFixed(2)}    `, 54));
-        receipt.push(alignLeftRight("   Totale scontato:", `${totalDiscounted.toFixed(2)}    `, 54));
+        receipt.push(core.alignLeftRight("   Sconto:", `${core.safeSubtraction(totalUndiscounted,totalDiscounted).toFixed(2)}    `, 54));
+        receipt.push(core.alignLeftRight("   Totale scontato:", `${totalDiscounted.toFixed(2)}    `, 54));
         receipt.push("");
-        receipt.push(alignLeftRight("   CODICE PROMO:", `${cartUser.promo.toUpperCase()}    `, 54));
-        receipt.push(frame("+", "-", 54));
+        receipt.push(core.alignLeftRight("   CODICE PROMO:", `${cartUser.promo.toUpperCase()}    `, 54));
+        receipt.push(core.frame("+", "-", 54));
         
     }
     
     receipt.push("");
     
-    receipt.push(frame("**", "-", 54));
+    receipt.push(core.frame("**", "-", 54));
     
-    receipt.push(`   ${capitalize(cartUser.firstName)} ${capitalize(cartUser.lastName)} ha un credito residuo di ${cartUser.wallet.toFixed(2)}`);
+    receipt.push(`   ${core.capitalize(cartUser.firstName)} ${core.capitalize(cartUser.lastName)} ha un credito residuo di ${cartUser.wallet.toFixed(2)}`);
     
-    receipt.push(frame("**", "-", 54));
+    receipt.push(core.frame("**", "-", 54));
     
     
     
--- core-cart.mjs
+++ core-cart.mjs
@@ -3,8 +3,39 @@ import {carts, products, promoCode as promoCodes, users} from "./dataset.mjs"; /
 import * as fs from "fs";
 import * as os from "os";
 
-const discountedPrice = (price, rate = 0.10) => (price * (1 - rate)).toFixed(2);
+
+
+// Mie funzioni
+
+const safeSum = (a, b) => ((a * 100) + (b * 100)) / 100; // Javascript e i decimali non vanno d'accordo.
+const safeSubtraction = (a, b) => ((a * 100) - (b * 100)) / 100;
+
+const capitalize = (str) => `${str[0].toUpperCase()}${str.substring(1).toLowerCase()}`;
+
+const frame = (sideStr, centralChar, width) => `${sideStr} ${centralChar.repeat( width - ((sideStr.length * 2) + 2) )} ${sideStr}`;
+const alignLeftRight = (strLeft, strRight, width) => strLeft + (" ".repeat(width-strLeft.length) + strRight).slice(-(width-strLeft.length));
+
+const getProductByEan = (ean) => products.find(product => product.ean === ean);
+const getUserByUuid = (uuid) => users.find(user => user.uuid === uuid);
+const getPromoByName = (name) => promoCodes.find(promoCode => promoCode.name === name);
+
+
+const discountedPrice = (price, discount) => safeSubtraction(price, price * discount);
+
+//const discountedPrice = (price, rate = 0.10) => (price * (1 - rate)).toFixed(2);
+
+
+
+// Export
 
 export {
-    discountedPrice,
+    safeSum,
+    safeSubtraction,
+    capitalize,
+    frame,
+    alignLeftRight,
+    getProductByEan,
+    getUserByUuid,
+    getPromoByName,
+    discountedPrice    
 };
