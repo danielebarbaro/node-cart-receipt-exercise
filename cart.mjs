5cba0efe5545fbbe486da8a0bcbabc8a8c574a96
Daniele Barbaro <barbaro.daniele@gmail.com>
2022-04-11 18:05:02 +0200

    Soluzione completa esercizio.

--- cart.mjs
+++ cart.mjs
@@ -1,14 +1,66 @@
-import {carts, products, promoCode, users} from "./dataset.mjs";
+import {carts, products} from "./dataset.mjs";
 import * as core from "./core-cart.mjs";
 
-console.log('Promo', promoCode);
-console.log('Products', products);
-console.log('Users', users);
-console.log('Cart', carts);
+const today = new Date().toDateString();
 
-console.log('Discount', core.discountedPrice(100, 0.2));
+for (let item of carts) {
+    let receipt = '';
+    // console.log(' CART CONTENT DEBUG:', item);
+    const {user: userUUID, products: cartContent} = item;
 
-for (let cartRow of carts) {
+    const user = core.getUser(userUUID);
+    const {wallet, promo, firstName, lastName} = user;
+    const userCartList = core.getProductByCart(cartContent)
 
-    // genera ricevuta
+    if (cartContent.length === 0) {
+        console.error(`>> ${firstName} ${lastName} ha un CARRELLO SENZA PRODOTTI`)
+        continue;
+    }
+
+    const cartTotal = core.sum(userCartList)
+    const {percentage: discountPercentage} = core.getUserDiscount(promo)
+
+    let total = core.discountedPrice(cartTotal, discountPercentage)
+    const discount = (cartTotal - total).toFixed(2);
+
+    if (total > wallet) {
+        console.error(`>> ${firstName} ${lastName} ha un credito di ${wallet.toFixed(2)} e non può concludere l'acquisto di ${total}.`)
+        continue;
+    }
+
+    receipt += core.createDelimiter('+');
+    receipt += core.shopName();
+    receipt += `\r\n${today}`;
+    receipt += core.createDelimiter();
+    userCartList.forEach(product => {
+        let productName = core.formatProductNameMultiple(product.name);
+        receipt += `\r\n   [${product.ean}]    ${productName}\t\t${product.price.toFixed(2)}`
+    }, receipt)
+
+    receipt += core.createDelimiter();
+    receipt += `\r\n   Totale:\t\t\t ${cartTotal}`
+    receipt += core.createDelimiter('+');
+    if (discountPercentage > 0) {
+        receipt += `\r\n   Sconto:\t\t\t ${discount}`
+        receipt += `\r\n   Totale Scontato:\t\t ${total}`
+        receipt += `\r\n\n   PROMO:\t\t\t ${user.promo}`
+        receipt += core.createDelimiter('+');
+    } else {
+        total = cartTotal;
+    }
+
+    receipt += `\r\n`;
+    receipt += core.createDelimiter('**');
+    receipt += `\r\n   ${firstName} ${lastName} ha un credito residuo di ${(wallet - total).toFixed(2)}`
+    receipt += core.createDelimiter('**');
+    receipt += `\r\n`;
+
+    core.printReceipt(receipt, `receipts/${userUUID}_receipt_${new Date().getTime()}.txt`)
+    // console.log(receipt)
 }
+
+console.info(`\n>> Controlla le ricevute stampate.`)
+
+
+
+
--- core-cart.mjs
+++ core-cart.mjs
@@ -1,10 +1,76 @@
-import {carts, products, promoCode, users} from "./dataset.mjs";
-
 import * as fs from "fs";
 import * as os from "os";
+import {carts, products, promoCode, users} from "./dataset.mjs";
 
 const discountedPrice = (price, rate = 0.10) => (price * (1 - rate)).toFixed(2);
 
+const formatProductName = (product) => product.toLowerCase().charAt(0).toUpperCase() + product.toLowerCase().slice(1);
+
+const sum = (products) =>  products
+    .map(item => item.price)
+    .reduce((prev, current) => prev + current, 0)
+    .toFixed(2);
+
+const formatProductNameMultiple = (name) => {
+    let nameWithSpaces = name.split(' ');
+
+    if (nameWithSpaces.length === 1) {
+        name = formatProductName(name);
+    } else {
+        let res = [];
+        for (let piece of nameWithSpaces) {
+            res.push(formatProductName(piece));
+        }
+        name = res.join(' ')
+    }
+
+    return name;
+};
+
+const formatProductsList = (products) => products.map(item => {
+    let {name} = item
+    let nameWithSpaces = name.split(' ');
+
+    if (nameWithSpaces.length === 1) {
+        item.name = formatProductName(name);
+    } else {
+        let res = [];
+        for (let piece of nameWithSpaces) {
+            res.push(formatProductName(piece));
+        }
+        item.name = res.join(' ')
+    }
+
+    return item;
+})
+
+const shopName = () => {
+    const cartCode = process.pid;
+    const cartName = os.userInfo();
+    const {username} = cartName;
+    return `\r\n${username.toUpperCase()} &art - ${cartCode}`;
+}
+
+const printReceipt = (content, filename) => fs.writeFileSync(filename, content);
+
+const createDelimiter = (openClose = '*', symbol = '-', times = 50) => `\r\n${openClose} ${symbol.repeat(times)} ${openClose}`;
+
+const getUser = (uuid) => users.find(user => user.uuid === uuid)
+
+const getProductByCart = (cartProducts) => products.filter(product => cartProducts.includes(product.ean))
+
+const getUserDiscount = (code) => promoCode.filter(promo => promo.name === code).shift() || 0
+
 export {
     discountedPrice,
+    formatProductName,
+    formatProductNameMultiple,
+    formatProductsList,
+    shopName,
+    printReceipt,
+    getUser,
+    getProductByCart,
+    getUserDiscount,
+    createDelimiter,
+    sum
 };
