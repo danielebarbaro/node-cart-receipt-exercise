cc75f37463befe7d9f8b5adf8b7b38005ced8245
Matteo <99264939+Matteo29-mar@users.noreply.github.com>
2022-04-12 17:07:44 +0200

    consegna2

--- cart.mjs
+++ cart.mjs
@@ -1,64 +1,101 @@
 import {carts, products, promoCode, users} from "./dataset.mjs";
 import * as core from "./core-cart.mjs";
-import { prependOnceListener } from "process";
-import { Console } from "console";
 
-console.log('Promo', promoCode);
-console.log('Products', products);
-console.log('Users', users);
-console.log('Cart', carts);
-console.log('Price', price);
 
-console.log('Discount', core.discountedPrice(100, 0.2));
+/*console.log(`Promo`, promoCode);
+console.log(`Products`, products);
+console.log(`Users`, users);
+console.log(`Cart`, carts);
+console.log(`Price`, price);*/
+//console.log(`Discount`, core.discountedPrice(100, 0.2));
 
 for (let cartRow of carts) {
+    
+    
+     console.log(`riga del carrello da stampare`, cartRow)
 
-    console.log('inizio del nodo')
+     let prodottoUtente = cartRow.products;
+     let UUIDCorrente = cartRow.user;
+     let TotaleOrdine =0;
+     
+     
+     
+     let ean =``;
+     let nomeProdotto=``;
+     let prezzoProdotto=``;
+     
+     console.log(`utente corrente`, UUIDCorrente, `\n`)
+     //console.log(`prodotti utente corrente`, prodottoUtente, `\n`)
+     
+     let user= core.getUser(UUIDCorrente);
+     
+     let nomeUtente = user.firstName +` `+ user.lastName;
+     let disponibilitaUtente= user.wallet;
+     let promoUtente = user.promo;
+
+     
+     if(prodottoUtente.length<1){
+         console.log(`${nomeUtente} non ha prodotti nel carrello `)
+     }
 
 
+     if(disponibilitaUtente>0){
+         console.log(`${nomeUtente} ha il portafoglio pieno`)
+         console.log(`utente si chiama:`, nomeUtente, `\n`);
+     console.log(`utente ha  disponibile`, disponibilitaUtente, ` Euro\n`);
+
+
+     }else {
+         console.log(`${nomeUtente} ha il portafoglio vuoto`)
+     }
+     
+     for(let cartRow of prodottoUtente){
+
+        let prodCorrente= core.getProduct(cartRow);
+        let ean = prodCorrente.ean;
+        let nomeProdotto=prodCorrente.name;
+        let prezzoProdotto=prodCorrente.price;
+        let rigaRicevuta = `\t[${ean}] \t\t ${nomeProdotto} \t ${prezzoProdotto}`
+        console.log(rigaRicevuta, `\n`);
+        TotaleOrdine-= prezzoProdotto;
+     }
+
+     if(promoUtente !== `` 
+        && promoUtente !== undefined
+        && promoUtente !==null){
+
+            let rate = core.getPercentagefromPromocode(promoUtente);
+
+     console.log(`${nomeUtente} ha ${rate} di  sconto `)
+         console.log(` \t codice rpromo \t\t${nomeUtente}`);
+         let discountedPriceValue = core.discountedPrice(TotaleOrdine, rate);
+         console.log(`discountPriceValue`, discountedPriceValue);
+     }
+     if(disponibilitaUtente <= TotaleOrdine){
+         console.log(`${nomeUtente} non ha abbastanza soldi`)
+     }
+    // genera ricevuta
+}
+/* 
     let cartUserUUID = cartRow.user;
     let products = cartRow.products;
-
     //const {user: cartUserUUID, products} = cartRow;
     const user = core.getUser(cartUserUUID);
     //const {uuid, firstName, lastName, wallet, isTeacher, promo} = user;
-    
     const portafoglio =user.wallet;
     const name=user.firstName;
     const surname=user.lastName;
-
-    console.log('${name} ${surname} ha un portafoglio di ${portafoglio} euro');
-    console.log('${name} ${surname} vorrebbe comprare', products);
-   
-
+    console.log(`${name} ${surname} ha un portafoglio di ${portafoglio} euro`);
+    console.log(`${name} ${surname} vorrebbe comprare`, products);
 for(let curretProduct of products){
-
-
-    console.log('voglio  estrarre', currentProduct);
+    console.log(`voglio  estrarre`, currentProduct);
     let prodotto = core.getProduct(currentProduct);
-    console.log('[${prodotto.ean}] ${prodotto.name} ${prodotto.price} ')
+    console.log(`[${prodotto.ean}] ${prodotto.name} ${prodotto.price} `)
 }
-
-
-console.log('fine del nodo')
     products.forEach(product =>   {
-
         let prodotto = core.getProduct(product);
-        console.log('prodotto', prodotto);
+        console.log(`prodotto`, prodotto);
     })
-   
    let prodottoCorrente = core.getProductByCart(productId);
-   
-
-
-   
-    // console.log("riga del carrello da stampare", cartRow)
     // console.log("UUID", uuid)
-    // console.log("Prodotti", prodotti )
-
-    // genera ricevuta
-
-
-
-
-}
+    // console.log("Prodotti", prodotti )*/
--- core-cart.mjs
+++ core-cart.mjs
@@ -7,29 +7,68 @@ console.log('MATTEOPC');
 
 const discountedPrice = (price, rate = 0.10) => (price * (1 - rate)).toFixed(2);
 
-const helloworld = (name => ' hello $ {name}');
+//arrow function
+//const getUser = (uuid) => users.find(user => user.uuid === uuid);
+const getUser = function (uuid){
+    let user = users.find(user => user.uuid === uuid);
+    return user;
+} 
+
+/* function getUser(uuid){
+    let user = user.find
+    return user;
+}*/
+//const getPRoduct = (productId) => products.find(product => product.ean === productId)
+
+/*function getProduct(productId){
+    let product = product.find(product => productId === product.ean);
+    return product;
+} funzioned ichiarativa*/
+
+const getProduct = function (productId){
+    return products.find(product => productId === product.ean)
+}
+
+
+const getPercentagefromPromocode = function (promoCodeName){
+   if(promoCodeName !==``
+    && promoCodeName !== undefined
+    && promoCodeName !==null){
+       
+       let rate = promoCode.find(promo => promoCodeName === promo.name);
+       console.log(rate)
+       return rate.percentage;
+    
+    
+    }
+    
+    return 0;
+}
+
+export {
+    discountedPrice,
+    getUser,
+    getPercentagefromPromocode,
+    getProduct
+};
+
+/*const helloworld = (name => ' hello $ {name}');
 
 const printShopName = () => {
     const {username} = os.userInfo();
     return '${name.toUpperCase()} - Cart ${process.pid}';
-
 }
-
-const getUser = (uuid) => users.find(user => user.uuid === uuid);
-
 //funzioni
 function nomeprodotto(product){
     const formatProductName = (product) => product;
     return formatProductName;
 }
 console.log(nomeprodotto, formatProductName);
-
 function tipo(prodfuct, Type){
     const filterType = (product, Type ) => product;
     return filterType;
 }
 console.log(tipo, filterType);
-
 function somma(product){
     const sumCartItem= (products) => (products);
     //ho dei dubbi per come farla
@@ -46,27 +85,6 @@ const printReceipt= (uuid, receipt) => {
 
 };
 const createDelimiter= (openClose, symbol, times) => '${openClose} ${symbol.repeat(times)} ${openClose}';
-
-
 const getUserDiscount = () =>0;
-
 const receiptFileName = (uuid, date) => '';
-
-
-const getPRoduct = (productId) => products.find(product => product.ean === productId)
-
-
-export {
-    discountedPrice,
-    helloworld,
-    printShopName,
-    getUser,
-    formatProductName,
-    filterType,
-    sumCartItem,
-    formatProductList,
-    printReceipt,
-    createDelimiter,
-    getUserDiscount,
-    receiptFileName
-};
+*/
