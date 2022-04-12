708401e7154001f6f6378129f9bc0059eb104948
Leonardo Cionchi <leonardo.cionchi@edu.itspiemonte.it>
2022-04-12 17:18:24 +0200

    Lavoro in classe 12/04/2022

--- cart.mjs
+++ cart.mjs
@@ -1,23 +1,84 @@
 import {carts, products, promoCode, users} from "./dataset.mjs";
 import * as core from "./core-cart.mjs";
-import { separatore } from "./core-cart.mjs";
+import { discountedPrice } from "./core-cart.mjs";
+import { getPercentageFromPromocode } from "./core-cart.mjs";
 
-console.log('Promo', promoCode);
-console.log('Products', products);
-console.log('Users', users);
-console.log('Cart', carts);
+// console.log('Promo', promoCode);
+// console.log('Products', products);
+// console.log('Users', users);
+// console.log('Cart', carts);
 
-console.log('Discount', core.discountedPrice(100, 0.2));
+//console.log('SHOP NAME: ', core.printShopName());
 
-for (let cartRow of carts) { 
-    console.log(separatore);
-    const {user: cardUserUUID, products}=cartRow;
-    const user = core.getUser(cartUserUUID);
-    const portafoglio = user.wallet;
-    const name = user.formatProductName.firstName;
-    const surname = user.formatProductName.lastName;
+for (let cartRow of carts) {
+    
 
-    console.log(creditoResiduo(user));
+    console.log('\t\tINIZIO DEL NODO')
+
+    //const {user:cartUserUUID, products} = cartRow;
+    let cartUserUUID = cartRow.user;
+    let products = cartRow.products;
+    let totOrdine = 0;
+    let user = core.getUser(cartUserUUID);
+    let promoUtente = user.promo;
+
+    let rate = core.getPercentageFromPromocode(promoUtente);
+
+    let ean = '';
+    let nomeProdotto = '';
+   
+    
+    //const {uuid, firstName, lastName, wallet, isTeacher, promo} = user;
+    const wallet = user.wallet;
+    const name = user.firstName;
+    const surname = user.lastName;
+
+    if(products.lenght > 0){
+        console.log(`${name} ${surname} vorrebbe comprare `, products);
+    }
+    else if(products.lenght = 0){
+        console.log(`${name} ${surname} ha il carrello vuoto`);
+        }
+    if(wallet > 0){
+        console.log(`${name} ${surname} ha ${wallet} euro`);
+    }
+    //products.forEach(product => {
+    for (let currentProduct of products) {
+        //console.log('Voglio estrarre', currentProduct);
+        let prodotto = core.getProduct(currentProduct);
+        console.log(`\t[${prodotto.ean}]\t${prodotto.name}\t\t${prodotto.price}`)
+        totOrdine += prodotto.price;
+        //console.log('Prodotto estratto con nome:', prodotto.price);
+    }
+
+    if(promoUtente !== ''
+        && promoUtente !== undefined
+        && promoUtente !== null){
+            console.log(`\t CODICE PROMO: \t\t ${promoUtente}`)
+            let discountedPriceValue = core.discountedPrice(totOrdine, rate);
+            console.log(`PRODOTTO SCONTATO\t\t`, discountedPriceValue)
+        }
+
+    if(wallet < discountedPrice){
+        console.log(`${discounteredPrice(totOrdine,getPercentageFromPromocode(promoCode.name))}`)
+    }
+    
+    console.log('\t\tFINE DEL NODO\n\n')
+    //})
+
+    // console.log('RIGA DEL CARRELLO DA STAMPARE', cartRow)
+    // console.log('UUID', uuid)
+    // console.log('Prodotti', products)
+    // console.log('User completo', user);
+
+    //trovare uno user dato il carrello
+
+
+    // capire se l'utente può comprare
+
+    // capire se l'utente ha delle promo
+
+    //...
 
     // genera ricevuta
 }
--- core-cart.mjs
+++ core-cart.mjs
@@ -2,88 +2,49 @@ import {carts, products, promoCode, users} from "./dataset.mjs";
 
 import * as fs from "fs";
 import * as os from "os";
-import { privateDecrypt } from "crypto";
-
-
-const separatore = `+ -------------------------------------------------- +`;
-export {
-    separatore
-}
-
-const listaProdotti = (prodotti) => {
-    let stringa = ``;
-    for(let item of prodotti){
-        if(item !== prodotti)
-    }
-}
-
-
-//const getTotale = (productsUser) => 
-
-
-
-
 
 const discountedPrice = (price, rate = 0.10) => (price * (1 - rate)).toFixed(2);
-export {
-    discountedPrice,
-};
 
+const helloWorld = (name => `Hello ${name}`);
 
-const printShopName = () =>{
-
+const printShopName = () => {
+    const {username} = os.userInfo();
+    return `${username.toUpperCase()} - Cart ${process.pid}`;
 }
 
-const getUser = (uuid) => users.find(users => users.uuid);
+const getUser = (uuid) => users.find(user => user.uuid === uuid);
 
-//let creditoResiduo(user)
-const stringFormat = (string) => product.toLowerCase().charAt(0).toUpperCase() + product.toLowerCase().slice(1)
+const getProduct = (productId) => products.find(product => product.ean === productId)
 
-const formatProductName = (product) => {
-
-for (let item of products) {
-    let formattedName = '';
-    let productName = item.name
-
-    let multiName = productName.split(' ');
-
-    if (multiName.length > 1) {
-        for (let piece of multiName) {
-            formattedName += `${format(piece)} `;
-        }
-    } else {
-        formattedName = format(productName);
+const getPercentageFromPromocode = function (promoCodeName) {
+    if(promoCodeName !== ''
+        && promoCodeName !== undefined
+        && promoCodeName !== null){
+            let rate = promoCode.find(item => promoCodeName === item.name);
+            return rate.percentage;
     }
-
-    item.name = formattedName;
-    
+    return 0;
 }
 
+const formatProductName = (product) => product;
 
-const filterType = (products) => products;
+const filterType = (products, type) => product;
 
-
-const sumCartItem = (carts.products) => 
-{
-    let total = 0;
-    for(let item of carts.products)
-        total += carts.products.price
-};
-
-
-const stampaTotale = (users) => {
-    console.log(`Totale:                ${sumCartItem()}`)
-}
-
+const sumCartItem = (products) => (products);
 
 const formatProductList = (products) => products;
-const printReceipt = (content, fileName) => ``;
-const createDelimiter = (openClose, symbol, times) => ``;
-const getProductByCart = (products) => products;
-const getUserDiscount = () => 0;
-const receiptFile = (uuid, date) => ``;
 
-export{
+const printReceipt = (content, filename) => '';
+
+const createDelimiter = (openClose, symbol, times) => ''
+
+const getProductByCart = (products) => products;
+
+const getUserDiscount = () => 0;
+
+const receiptFileName = (uuid, date) => '';
+
+export {
     discountedPrice,
     helloWorld,
     printShopName,
@@ -94,6 +55,9 @@ export{
     formatProductList,
     printReceipt,
     createDelimiter,
-
-
-}
+    getProductByCart,
+    getUserDiscount,
+    receiptFileName,
+    getProduct,
+    getPercentageFromPromocode
+};
