3ee908be965e6ff8e4369b135a81f2cee3aa2770
Cat-Genova <100271672+Cat-Genova@users.noreply.github.com>
2022-04-12 17:40:20 +0200

    Consegna compito cart e core-cart
    Esercitazione cart e core-cart

--- core-cart.mjs
+++ core-cart.mjs
@@ -1,10 +1,86 @@
-import {carts, products, promoCode, users} from "./dataset.mjs";
+import {carts, products, promoCode, users} from "./dataset.mjs"; //
 
-import * as fs from "fs";
-import * as os from "os";
+import * as fs from "fs"; //file system (fs)
+import * as os from "os"; //sistema operativo (os)
 
 const discountedPrice = (price, rate = 0.10) => (price * (1 - rate)).toFixed(2);
 
+const helloWorld = (name => `Hello ${name}`);
+
+//ARROW FUNCTION : const getUser = (uuid) => users.find(user => user.uuid === uuid);
+
+//funzione iterativa
+function getUser(uuid){
+    let user = users.find(user => uuid === user.uuid);
+    return user;
+}
+
+
+//function getProduct(productId){
+  //  let product = products.find(product => productId === product.ean);
+    //return product;
+//}
+
+/*const getProduct = function (productId){
+     
+    return products.find(product => productId === product.ean);
+}*/
+
+const getProduct = (productId) => products.find(product => product.ean === productId)
+
+const getPercentageFromPromoCode = function (promoCodeName){
+
+    if(promoCodeName !== '' 
+       && promoCodeName !== undefined
+       && promoCodeName !== null) {
+
+    let rate = promoCode.find(item => promoCodeName === item.name);
+    return rate.percentage;
+    }
+    return 0;
+    
+    
+}
+
+
+const printShopName = () => {
+    const {username} = os.userInfo();
+    return `${username.toUpperCase()} - Cart ${process.pid}`;
+}
+
+
+const formatProductName = (product) => product;
+
+const filterType = (products, type) => product;
+
+const sumCartItem = (products) => (products);
+
+const formatProductList = (products) => products;
+
+const printReceipt = (content, filename) => '';
+
+const createDelimiter = (openClose, symbol, times) => ''
+
+const getProductByCart = (products) => products;
+
+const getUserDiscount = () => 0;
+
+const receiptFileName = (uuid, date) => '';
+
 export {
     discountedPrice,
+    helloWorld,
+    printShopName,
+    getUser,
+    formatProductName,
+    filterType,
+    sumCartItem,
+    formatProductList,
+    printReceipt,
+    createDelimiter,
+    getProductByCart,
+    getUserDiscount,
+    receiptFileName,
+    getProduct,
+    getPercentageFromPromoCode
 };
