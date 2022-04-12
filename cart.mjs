55ce4ed8459e240ce9e4416e48031923c992f721
AssiaSam <assia.samime@edu.itspiemonte.it>
2022-04-12 17:52:23 +0200

    Samime-consegna

--- cart.mjs
+++ cart.mjs
@@ -1,14 +1,75 @@
 import {carts, products, promoCode, users} from "./dataset.mjs";
 import * as core from "./core-cart.mjs";
 
-console.log('Promo', promoCode);
-console.log('Products', products);
-console.log('Users', users);
-console.log('Cart', carts);
+// console.log('Promo', promoCode);
+// console.log('Products', products);
+// console.log('Users', users);
+// console.log('Cart', carts);
 
-console.log('Discount', core.discountedPrice(100, 0.2));
+//console.log('SHOP NAME: ', core.printShopName());
 
-for (let cartRow of carts) {
 
-    // genera ricevuta
+for (let cartRow of carts) {// per tutti i carrelli fammi vedere la riga dei carrelli 
+    console.log(' INIZIO DEL NODO')
+
+     //console.log(?RIGA DEL CARRELLO DA STAMPARE', ittem, '\n')
+     let prodottiUtente= cartRow.products;
+     let UUIDCorrente = cartRow.user;
+     let totaleOrdine = 0;
+   
+
+     let ean = '';
+     let nomeProdotto = '';
+     let prezzoProdotto ='';
+     let rigaRicevuta = '';
+
+
+     console.log('Utente corrente', UUIDCorrente, '\n')
+
+     let user = core.getUser(UUIDCorrente);
+     console.log('Utente completo: ', user, '\n');
+     let nomeUtente = user.firstName + ' '+ user.lastName;
+     let disponibilitaUtente = user.wallet;
+    let promoUtente = user.promo;
+
+    let rate = core.getPercentageFromPromoCode(promoUtente);
+
+    console.log(`${nomeUtente} ha IL CODICE PROMO  ${promoUtente}`)
+    console.log(`${nomeUtente} ha ${rate} di sconto`)
+
+
+     if (prodottiUtente.lenght < 1){
+         console.log(`${nomeUtente} NON HA PRODOTI NEL CARRELLO.`)
+     }
+     if (disponibilitaUtente>0){
+         console.log(`${nomeUtente} ha il portafoglio pieno.`)
+         console.log ('Utente si chiama : ', nomeUtente, '\n');
+         console.log('Utente ha disponibile: ', disponibilitaUtente, 'Euro \n')
+        } else{
+         console.log(`${nomeUtente} ha il portafoglio vuoto.`)
+     }
+
+     for(let item of prodottiUtente){
+         let prodCorrente = core.getProduct(item);
+         let ean = prodCorrente.ean;
+         let nomeProdotto= prodCorrente.name;
+         let prezzoProdotto= prodCorrente.price;
+         console.log(`\t [${ean}] \t\t ${nomeProdotto} \t ${prezzoProdotto}`);
+        console.log(rigaRicevuta,'\n')
+        totaleOrdine += prezzoProdotto;
+        }
+        
+        if (promoUtente !== ''
+        && promoUtente !== undefined
+        && promoUtente !== null){
+            console.log(`\t CODICE PROMO: \t \t  ${promoUtente}`);
+        let discountedPriceValue = core.discountedPrice(totaleOrdine, rate);
+        console.log('discountedPriceValue', discountedPriceValue);  
+       }
+
+
+        if (disponibilitaUtente < totaleOrdine){
+            console.log(`${nomeUtente} NON HA ABBASTANZA SOLDI PER COMPRARE.`)
+        }
+
 }
--- core-cart.mjs
+++ core-cart.mjs
@@ -5,6 +5,92 @@ import * as os from "os";
 
 const discountedPrice = (price, rate = 0.10) => (price * (1 - rate)).toFixed(2);
 
+const helloWorld = (name => `Hello ${name}`);
+
+const printShopName = () => {
+    const {username} = os.userInfo();
+    return `${username.toUpperCase()} - Cart ${process.pid}`;
+}
+//funzione getUser dichiartiva 
+function getUser(uuid) {
+    let user = users.find(user =>uuid === user.uuid);
+    return user; 
+}
+
+//funzione getUser anonima
+/*const getUser=function (uuid){
+    let user = users.find(user =>uuid === user.uuid);
+    return user;
+}*/
+
+//funzione getProduct dichiarativa 
+/*function getProduct(productId){
+    let product = product.find(product => productId === product.ean)
+    return product;
+}*/
+
+//funzione getProduct anonima
+const getProduct= function (productId){
+    return products.find(product => productId === product.ean)
+}
+//funzione getPercentageFromPromoCode dichiarativa
+/* 
+    function getPercentageFromPromoCode(promoCodeName){
+        if (promoCodeName === ''
+        && promoCodeName !== undefined
+        && promoCodeName !== null){
+    
+
+        let rate = promoCode.find(item => promoCode === item.name);
+        return rate.persentage;
+    }
+    return 0;
+}
+*/
+// funzione getPercentageFromPromoCode anonima
+const getPercentageFromPromoCode = function (promoCodeName){
+
+    if(promoCodeName !== '' 
+       && promoCodeName !== undefined
+       && promoCodeName !== null) {
+
+    let rate = promoCode.find(item => promoCodeName === item.name);
+    return rate.percentage;
+    }
+    return 0;
+}
+const formatProductName = (product) => product;
+
+const filterType = (products, type) => product;
+
+const sumCartItem = (products) => (products);
+
+const formatProductList = (products) => products;
+
+const printReceipt = (content, filename) => '';
+
+const createDelimiter = (openClose, symbol, times) => ''
+
+const getProductByCart = (products) => products;
+
+const getUserDiscount = () => 0;
+
+const receiptFileName = (uuid, date) => '';
+
 export {
     discountedPrice,
+    helloWorld,
+    printShopName,
+    getUser,
+    formatProductName,
+    filterType,
+    sumCartItem,
+    formatProductList,
+    printReceipt,
+    createDelimiter,
+    getProductByCart,
+    getUserDiscount,
+    receiptFileName,
+    getProduct,
+    getPercentageFromPromoCode
 };
