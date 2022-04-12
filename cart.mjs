fb1e7e0379d1044a044b54440e81e02a63d5c622
MrcllBo <100216203+MrcllBo@users.noreply.github.com>
2022-04-12 23:12:48 +0200

    Aggiornamento esercizio

--- cart.mjs
+++ cart.mjs
@@ -1,16 +1,27 @@
 import {carts, products, promoCode, users} from "./dataset.mjs";
 import * as core from "./core-cart.mjs";
 
-console.log('Promo', promoCode);
-console.log('Products', products);
-console.log('Users', users);
-console.log('Cart', carts);
+//console.log('Promo', promoCode);
+//console.log('Products', products);
+//console.log('Users', users);
+//console.log('Cart', carts);
 
-console.log('Discount', core.discountedPrice(100, 0.2));
+//console.log('Discount', core.discountedPrice(100, 0.2));
 
 for (let cartRow of carts) {
 
+
+    let UUIDCorrente=cartRow.user;
+    let prodottiUtente=cartRow.products;
+    //console.log(UUIDCorrente);
     // genera ricevuta
+    
+    let user = core.getUser(cartRow.user);
+
+
+
+    console.log(user,'\n');
+    console.log(prodottiUtente,'\n');
+    
 }
 
-console.log('Marcccc');
--- core-cart.mjs
+++ core-cart.mjs
@@ -2,9 +2,40 @@ import {carts, products, promoCode, users} from "./dataset.mjs";
 
 import * as fs from "fs";
 import * as os from "os";
+import { randomUUID } from "crypto";
 
 const discountedPrice = (price, rate = 0.10) => (price * (1 - rate)).toFixed(2);
 
+//arrow function
+/*const getUser=(uuid)=>users.find(user=>user.uuid===uuid);*/
+//funzione dichiarativa
+/*function getUser(uuid){
+    let user=user.find(user=>uuid===user.uuid);
+    return user;
+}*/
+//funzione anonima
+const getUser=function(uuid){
+    let user=users.find(user=>uuid===user.uuid);
+    return user;
+}
+
+
+//arrow function
+/*const getProduct=(ean)=>products.find(product=>product.ean===ean);*/
+//funzione dichiarativa
+/*function getProduct(ean){
+    let product=product.fint(product=>ean===product.ean);
+    return product;
+}*/
+//funzione anonima
+const getProduct=function(ean){
+    let product=product.fint(product=>ean===product.ean);
+    return product;
+}
+
+
 export {
     discountedPrice,
+    getUser,
+    getProduct,
 };
