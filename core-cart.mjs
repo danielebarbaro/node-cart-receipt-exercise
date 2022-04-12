6328e471eaddadfca8c92908f7ae119a4533e6ab
nicolotti22 <100216160+nicolotti22@users.noreply.github.com>
2022-04-12 17:49:20 +0200

    consegna cart e code-cart

--- core-cart.mjs
+++ core-cart.mjs
@@ -5,6 +5,49 @@ import * as os from "os";
 
 const discountedPrice = (price, rate = 0.10) => (price * (1 - rate)).toFixed(2);
 
+const helloWorld = (name => `Hello ${name}`);
+
+const printShopName = () => {
+    const {username} = os.userInfo();
+    return `${username.toUpperCase()} - Cart ${process.pid}`;
+}
+
+const getUser = (uuid) => users.find(user => user.uuid === uuid);
+
+const getProduct = (productId) => products.find(product => product.ean === productId)
+
+const formatProductName = (product) => product;
+
+const filterType = (products, type) => product;
+
+const sumCartItem = (products) => (products);
+
+const formatProductList = (products) => products;
+
+const printReceipt = (content, filename) => '';
+
+const createDelimiter = (openClose, symbol, times) => ''
+
+const getProductByCart = (products) => products;
+
+const getUserDiscount = () => 0;
+
+const receiptFileName = (uuid, date) => '';
+
 export {
     discountedPrice,
+    helloWorld,
+    printShopName,
+    getUser,
+    formatProductName,
+    filterType,
+    sumCartItem,
+    formatProductList,
+    printReceipt,
+    createDelimiter,
+    getProductByCart,
+    getUserDiscount,
+    receiptFileName,
+    getProduct
 };
+
