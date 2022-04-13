d5a2d9f001fa3c50de70dcecb1efb45ecbc5beb0
Simo-stella-31 <100216169+Simo-stella-31@users.noreply.github.com>
2022-04-13 13:08:25 +0200

    Allineamento esercizio del 12/4/2022 Simone Stella

--- cart.mjs
+++ cart.mjs
@@ -1,14 +1,82 @@
 import {carts, products, promoCode, users} from "./dataset.mjs";
-import * as core from "./core-cart.mjs";
+import * as core from "./core-cart.mjs";    /*In questo file tutte le funzioni le chiamiamo core*/
 
-console.log('Promo', promoCode);
+/*console.log('Promo', promoCode);
 console.log('Products', products);
 console.log('Users', users);
-console.log('Cart', carts);
+console.log('Cart', carts);*/
 
-console.log('Discount', core.discountedPrice(100, 0.2));
+/*console.log('Discount', core.discountedPrice(100, 0.2));*/
 
-for (let cartRow of carts) {
+for (let item of carts) {
 
-    // genera ricevuta
+    /*console.log("INIZIO DEL NODO");*/
+
+    /*console.log("RIGA DEL CARRELO DA STAMPARE", cartRow, "\n");*/
+
+    let prodottiUtente = item.products;
+    let UUIDCorrente = item.user;
+    let totaleOrdine = 0;
+
+    let ean = '';
+    let nomeProdotto = '';
+    let prezzoProdotto = '';
+
+    /*console.log("Utente corrente", UUIDCorrente, "\n");
+    console.log("Prodotti utente corrente", prodottiUtente, "\n");*/
+
+    let user = core.getUser(UUIDCorrente);
+
+    console.log("Utente completo: ", user, "\n");
+
+    let nomeUtente = `${user.firstName} ${user.lastName}`;
+    let disponibilitaUtente = user.wallet;
+    let promoUtente = user.promo;
+
+    let rate = core.getPercentageFromPromoCode(promoUtente);
+    
+    if(prodottiUtente.length < 1)
+    {
+        console.log(`${nomeUtente} NON HA PRODOTTI NEL CARRELLO`)
+    }
+
+    if(disponibilitaUtente > 0)
+    {
+        /*console.log(`${nomeUtente} ha il portafoglio pieno\n`)*/
+        console.log("Utente si chiama: ", nomeUtente, "\n");
+        console.log("Utente ha disponibilità: ", disponibilitaUtente, " euro\n");
+    }
+    else
+    {
+        console.log(`${nomeUtente} ha il portafoglio vuoto`);
+    }
+
+    for(let item of prodottiUtente)
+    {
+        let prodCorrente = core.getProduct(item);
+        let ean = prodCorrente.ean;
+        let nomeProdotto = prodCorrente.name;
+        let prezzoProdotto = prodCorrente.price;
+        let rigaRicevuta = `\t [${ean}] \t\t ${nomeProdotto} \t\t\t ${prezzoProdotto}`;
+
+        console.log(rigaRicevuta, "\n");
+
+        totaleOrdine += prezzoProdotto;
+    }
+
+    if(promoUtente !== '' && promoUtente !== undefined && promoUtente !== null)
+    {
+        console.log(`\t CODICE PROMO: \t\t ${promoUtente}`);
+
+        let discountedPriceValue = core.discountedPrice(totaleOrdine, rate);
+
+        console.log(`discountedPriceValue`, discountedPriceValue);
+    }
+
+    if(disponibilitaUtente < totaleOrdine)
+    {
+        console.log(`${nomeUtente} NON HA ABBASTANZA SOLDI PER COMPRARE`);
+    }
+
+    /*Fare la somma*/
 }
--- core-cart.mjs
+++ core-cart.mjs
@@ -1,10 +1,49 @@
 import {carts, products, promoCode, users} from "./dataset.mjs";
 
-import * as fs from "fs";
-import * as os from "os";
+import * as fs from "fs";   /*File system*/
+import * as os from "os";   /**/
 
 const discountedPrice = (price, rate = 0.10) => (price * (1 - rate)).toFixed(2);
 
+/*const discountedPrice = function (price, rate = 20.0){
+
+    let result = price - ((price * rate) / 100.0);
+
+    return result;
+}*/
+
+/*const getUser = (uuid) => users.find(user => user.uuid === uuid);*/
+
+const getUser = function (uuid){
+
+    let user = users.find(user => uuid === user.uuid);
+
+    return user;
+}
+
+/*const getProduct = (productId) => products.find(product => productId === product.ean);*/
+
+const getProduct = function(productId){
+
+    return products.find(product => productId === product.ean);
+}
+
+const getPercentageFromPromoCode = function(promoCodeName){
+
+    if(promoCodeName !== '' && promoCodeName !== undefined && promoCodeName !== null)
+    {
+        let rate = promoCode.find(item => promoCodeName === item.name);
+
+        return rate.percentage;
+    }
+
+    return 0;
+}
+
 export {
+
     discountedPrice,
+    getUser,
+    getProduct,
+    getPercentageFromPromoCode
 };
