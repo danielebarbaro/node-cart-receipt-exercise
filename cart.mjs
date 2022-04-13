9b09dcfdcaf87864e326f80893311519c5b65b9a
Rdx <100215987+Rdxv@users.noreply.github.com>
2022-04-13 13:12:32 +0200

    Esercizio aggiornato al 12 aprile 2022

--- cart.mjs
+++ cart.mjs
@@ -1,31 +1,84 @@
-import {carts, products, promoCode, users} from "./dataset.mjs";
-import * as core from "./core-cart.mjs";
-/*
+import {carts, products, promoCode, users} from "./dataset.mjs"; // i dati sono presenti in dataset.mjs
+import * as core from "./core-cart.mjs";                        // i metodi e ciò che è presente in core-cart.mjs può essere richiamato in cart.mjs scrivendo "core" prima del nome 
+                                                               // della cosa da richiamare. Un po' come "ereditarli" da un' altra classe
 console.log('Promo', promoCode);
 console.log('Products', products);
 console.log('Users', users);
 console.log('Cart', carts);
-*/
-//console.log('SHOP NAME', core.printShopName());
 
-for (let cartRow of carts) {
-    const {user: cartUserUUID, products} = cartRow;
-    const user = core.getUser(uuid);
-    const {uuid, firstName, lastName, wallet, isTeacher, promo} = user;
+console.log('Discount',core.discountedPrice(200, 0.20));
 
-    //console.log('RIGA DEL CARRELLO DA STAMPARE', cartRow)
-    //console.log('UUID', uuid)
-    //console.log('Prodotti', products)
-    //console.log('User complete', user);
+for (let cartRow of carts) {        // of vuol dire "fammi vedere l' oggetto del carrello". Se andiamo a vedere il file dataset vediamo che ogni cart contiene id utente e prodotti. 
+    console.log('INIZIO DEL NODO')                          // questo for cicla i carrelli e stampa quelle proprietà
+    console.log('Riga del carrello da stampare', cartRow); // cartRow si riferisce alla riga del carrello che stiamo ciclando (nell' insieme dei carts)
 
- // trovare uno user dato il carrello
+    let prodottiUtente = cartRow.products; // prodottiUtente è un array di prodotti
+    let UUIDCorrente = cartRow.user;       // per ogni oggetto nel carrello voglio accedere alle proprietà dell' utente e dei prodotti. E salvarle in 2 variabili: UUIDCorrente e ProdottiUtente
+    let totaleOrdine = 0;                 // inizializzo il totale ordine a 0
+    
 
+    let ean ='';
+    let nomeProdotto = '';
+    let prezzoProdotto = '';
 
- // capire se l' utente può comprare
+    //console.log('Utente corrente', UUIDCorrente,'\n');
+    //console.log('Prodotti utente corrente', prodottiUtente, '\n');
 
- // capire se l' utente ha delle promo
+    let user = core.getUser(UUIDCorrente); // uso il metodo getUser che ho creato in core-cart.mjs. getUser serviva a restituire tutti i dati dell' utente che corripsonde all' id passato.
+    //console.log('Utente completo', user);
 
+    //let nomeUtente = `${user.firstName} + '' + ${user.lastName}`; // come la riga sotto ma scritta coi backtick
+    let nomeUtente = user.firstName + '' + user.lastName; // creo una variabile che contiene il nome e cognome dell' utente. Scritta con i + come facciamo in java e C#
+    let disponibilitaUtente = user.wallet;
+    let promoUtente = user.promo;
+    
 
+    
+    
 
-    // genera ricevuta
+    //console.log(`${nomeUtente} ha un codice sconto `);
+    //console.log(`${nomeUtente} ha un buono sconto del ${rate}%`);
+
+    if(prodottiUtente.length < 1){ // se il carrello dell' utente ha prodotti, può acquistare. il .length abbinato ad un array (come prodottiUtente ritorna il numero di righe contenute in un array. I prodotti nel carrello erano tra []. una riga = 1 prodotto.
+    console.log(`${nomeUtente} non ha prodotti nel carrello.`);
+    }
+
+    if(disponibilitaUtente > 0){
+        console.log(`${nomeUtente} ha il portafoglio pieno`);
+        console.log('Utente si chiama: ', nomeUtente, '\n');
+        console.log('Utente ha disponibile: ', disponibilitaUtente, 'Euro \n');
+    
+    }else{
+        console.log(`${nomeUtente} ha il portafoglio vuoto`);
+    }
+
+    for (let item of prodottiUtente ) {
+        let prodCorrente = core.getProduct(item); // uso il metodo getProduct che ho creato in core-cart.mjs. getProduct serviva a restituire tutti i dati del prodotto che corripsonde all' id passato.
+        console.log(prodCorrente);                                                  
+         ean = prodCorrente.ean;                     // ean di un prodotto. ad es: [120193]
+         nomeProdotto = prodCorrente.name;           // nome di un prodotto ad es: Alpi
+         prezzoProdotto = prodCorrente.price;        // prezzo di un prodotto ad es: 22.10
+        let rigaRicevuta = `\t [${ean}] \t\t ${nomeProdotto} \t ${prezzoProdotto}`; // creo una riga che contiene l' ean, il nome e il prezzo del prodotto.
+        totaleOrdine += prezzoProdotto;              // aggiungo il prezzo del prodotto all' importo totale dell' ordine.
+
+        if (promoUtente  !== '' 
+        && promoUtente !== undefined 
+        && promoUtente !== null) {
+            let rate = core.getPercentageFromPromoCode(promoUtente); // uso il metodo getPercentageFromPromoCode che ho creato in core-cart.mjs. getPercentageFromPromoCode serviva a restituire il valore del buono sconto promozionale passato come parametro.
+            console.log(`\t Codice promo: \t\t ${promoUtente}`);
+            let discountedPriceValue = core.discountedPrice(totaleOrdine, rate);
+            console.log(` Prezzo scontato:`, discountedPriceValue);
+        }
+         
+
+        console.log(rigaRicevuta, '\n');
+
+        if (disponibilitaUtente < totaleOrdine) {
+            console.log(`${nomeUtente} non ha abb. soldi per comprare`);
+        
+        }
+        
+        // per fare le righe per delimitare gli scontrini *--------* e +--------+ usare la funzione .repeat
+    }
+    
 }
--- core-cart.mjs
+++ core-cart.mjs
@@ -3,22 +3,96 @@ import {carts, products, promoCode, users} from "./dataset.mjs";
 import * as fs from "fs";
 import * as os from "os";
 
-const discountedPrice = (price, rate = 0.10) => (price * (1 - rate)).toFixed(2);
+//const discountedPrice = (price, rate = 0.10) => (price * (1 - rate)).toFixed(2);
 
-const helloworld = (name => `Hello ${name}`);
+function discountedPrice(price, rate = 0.10) {
+    return (price * (1 - rate));
+}
 
+//const getUser = (uuid) => users.find(user => user.uuid === uuid);
+
+
+/*function getUser(uuid) {
+    let user = users.find(user => user.id === uuid);
+}
+*/
+
+const getUser = function(uuid) { // ci serve per ottenere i dati dell' utente, se viene trovato un utente con quell' id, allora restituisce i dati utente, altrimenti restituisce undefined
+    let user = users.find(user => uuid === user.uuid);
+    return user;
+}
+
+// 3 modi diversi di scrivere la stessa funzione
+// const getProduct = (productId) => products.find(product => product.id === product.ean); // arrow function
+
+/*
+function getProduct(productId) { //funzione dichiarativa
+    let product = products.find(product => product.id === product.ean); 
+    return product;
+}
+*/
+
+const getProduct = function (productId){ // funzione anonima. getProduct serve a verificare che nell' elenco dei prodotti ci sia un prodotto con id uguale a quello passato come parametro. L' id nell' elenco dei prodotti si chiama ean
+    return products.find(product => productId === product.ean);
+}
+
+/*
+function getPercentageFromPromoCode (promoCode) {
+    let rate = promoCode.find(promo => promoCode === item.promo);
+    return rate;
+}
+*/
+
+const getPercentageFromPromoCode = function (promoCodeName) {
+    if (promoCodeName !== '' && // controlla se promoCodeName  esiste ma è vuota
+    promoCodeName !== undefined && // controlla se promoCodeName non è undefined
+    promoCodeName !== null) { // controlla se promoCodeName non è null
+        
+        let rate = promoCode.find(item => promoCodeName === item.name);
+        return rate.percentage; // restituisce il valore del campo percentage come numero decimale es 0.10, se i 3 controlli precedenti sono veri
+    }
+    return 0; // restituisce 0 se uno dei controlli precedenti sono falsi
+}
+
+
+
+/*
 const printShopName = () => {
     const { username } = os.userInfo();     // ottiene il nome utente della macchina
     return `${machineName.toUpperCase()} = cart ${pid}`;
 } 
  
- const getUser = (uuid) => users.find(user => user.uuid === uuid);
  
- const formatProductName = (product) => product;
  
+
+ const formatProductName = (product) => {                               //metodo che trasforma le stringhe in minuscolo e poi fa diventar maiuscolo il primo carattere (pos. 0)
+    return product.toLowerCase().charAt(0).toUpperCase() + product.toLowerCase().slice(1) // usa anche lo slice per copiare la stringa dalla pos. 1 in poi
+ 
+ for (let item of products) {
+    let formattedName ='';
+    let productName = item.name
+
+    let multiName = productName.split('');                             // divido la stringa in corrispondenza di un separatore che in questo caso è lo spazio tra le parole
+    console.log('multiName',multiName);
+
+    if(multiName.length < 1){
+        for (let piece of multiName) {
+            formattedName += `${format(piece)} `;
+        }
+    } else {
+        formattedName = format(productName);
+    }
+    item.name = formattedName;
+
+    cart.push(item)
+   }
+}
+
  const filterType = (products, type) => product;
 
- const sumCartItem = (products, type) => (products);
+ const sumCartItem = (products, type) => (products.reduce(current, prod) )
+ current + (prod.type === type ? prod.price * (1 - rate) : (prod.price), 0);
+
 
  const formatProductList = (products) => (products);
  
@@ -31,17 +105,17 @@ const printShopName = () => {
  const getUserDiscount = () => 0;
 
  const receiptFileName = (uuid, date) => ``;
-    
-
-    
-    
+*/ 
+      
 
 
 export {
     discountedPrice,
+    getPercentageFromPromoCode,
+    getProduct,
+    getUser /*
     helloworld,
     printShopName,
-    getUser,
     formatProductName,
     filterType,
     sumCartItem,
@@ -50,5 +124,6 @@ export {
     createDelimiter,
     getProductByCart,
     getUserDiscount,
-    receiptFileName,
+    receiptFileName,*/
 };
+// sarebbe più corretto scrivere tutti i metodi in questo file e importarli in cart.mjs, anche se si possono scrivere tutti in cart.mjs (per capire meglio)  
