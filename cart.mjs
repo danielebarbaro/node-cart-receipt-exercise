2d368268aea726e1cd34f1e108920ca0230ca9d3
Lorenzo Avondo <99265034+LorenzoAvondo@users.noreply.github.com>
2022-04-13 23:37:17 +0200

    Consegna Compito

--- cart.mjs
+++ cart.mjs
@@ -1,14 +1,91 @@
 import {carts, products, promoCode, users} from "./dataset.mjs";
 import * as core from "./core-cart.mjs";
+import * as fs from "fs";
+/*receipt +=('Promo', promoCode);
+receipt +=('Products', products);
+receipt +=('Users', users);
+receipt +=('Cart', carts);*/
 
-console.log('Promo', promoCode);
-console.log('Products', products);
-console.log('Users', users);
-console.log('Cart', carts);
+//receipt +=('Discount', core.discountedPrice(100, 0.2));
+//receipt +=(core.calculateDiscountedPrice(100,0.2));
 
-console.log('Discount', core.discountedPrice(100, 0.2));
+let receipt = ''
+//per ogni carts fammi vedere cartRow, per ogni carrello voglio uno scontrino
+for(let item of carts){
+    
 
-for (let cartRow of carts) {
+    //receipt +=('Riga del carrello da stampare', item, '\n')
+    let prodottiUtente = item.products;
+    let UUIDCorrente = item.user;
+    let creditoResiduo = 0;
+    let totOrdine = 0;
+    
+    let user = core.getUser(UUIDCorrente)
+    let promoUtente = user.promo
+    let rate = core.getPercentFromPromocode(promoUtente)
+    let nomeUtente = user.firstName  +' '+ user.lastName;
+    let disponibilitaUtente = user.wallet;
+    
+    receipt=''
 
-    // genera ricevuta
+
+    // inizio
+    
+        receipt +=core.divisorio('+',55) + '\n'
+        receipt += core.printShopName() + '\n'
+        receipt += core.formatData() + '\n'
+       
+    
+    
+
+        if (prodottiUtente.lenght<1){
+            receipt +=nomeUtente + 'ha il carrello vuoto'
+
+        }   
+    
+        if (disponibilitaUtente>0){
+            receipt +='\n' +'Nome Utente:'+ nomeUtente + '\n'
+       
+            receipt +='Credito Utente:'+ disponibilitaUtente +'\n'
+
+        }
+        else{
+            receipt +=nomeUtente + 'ha il portafoglio vuoto.\n'
+        }
+   
+        receipt +=core.divisorio('*',55)+'\n';
+    
+        for(let item of prodottiUtente){
+            let prodCorrente= core.getProduct(item)
+            let ean = prodCorrente.ean;
+            let nomeProdotto = prodCorrente.name;
+            let prezzoProdotto = prodCorrente.price;
+            let rigaRicevuta = `\t[${ean}]\t${nomeProdotto}\t${prezzoProdotto}`
+            totOrdine +=  prezzoProdotto;
+            receipt += rigaRicevuta +'\n'
+    }   
+    receipt +=core.divisorio('*',55)+'\n';
+        receipt += '\tTotale Odine:\t\t' +  totOrdine.toFixed(2) + '\n'
+    receipt += core.divisorio('+',55) + '\n';
+    if(promoUtente !== '' && promoUtente !== undefined && promoUtente !== null){
+            receipt +=`\tCodice Promo:\t ${promoUtente}\n`
+            receipt +=`\tSconto:\t ${rate}\n`
+            let discountetPriceValue = core.discountedPrice(totOrdine, rate)
+            receipt +=`\tProdotto Scontato:\t ${discountetPriceValue}\n`
+            creditoResiduo = disponibilitaUtente - discountetPriceValue
+      
+    } else{
+           creditoResiduo = disponibilitaUtente - totOrdine;
+        }
+        receipt += core.divisorio('+',55)+'\n';
+        receipt += core.divisorio('**',55)+'\n';
+        if(creditoResiduo>=0)
+            receipt += `\t${nomeUtente} ha un credito residuo di ${creditoResiduo.toFixed(2)}\n`
+        else
+            receipt +='Credito non sufficiente' + '\n'
+        receipt += core.divisorio('**',55,)+'\n\n'
+    
+    //$UserUUID_receipt_$DATAORAGENERAZIONE.txt
+    let path =process.cwd()+'/receipts/' + UUIDCorrente + '_' + 'receipt' + '_' +new Date().toDateString()+'.txt';
+    fs.writeFile(path,receipt,(err) => { if (err) throw err; })
 }
--- core-cart.mjs
+++ core-cart.mjs
@@ -3,8 +3,95 @@ import {carts, products, promoCode, users} from "./dataset.mjs";
 import * as fs from "fs";
 import * as os from "os";
 
-const discountedPrice = (price, rate = 0.10) => (price * (1 - rate)).toFixed(2);
+const discountedPrice = (price, rate) => (price * (1 - rate)).toFixed(2);
 
-export {
+const calculateDiscountedPrice = function (price, rate){
+    return price * (1-rate);
+}
+//cerchiamo lo user per uuid, perciò la freccia qui sotto.
+const getUser = (uuid)=> users.find(user=>user.uuid === uuid);
+//ean lo trovi nel dataset dove ci sono i prodotti
+const getProduct = (productId) => products.find(product => product.ean === productId)
+
+const printShopName = () =>{
+    const machine = os.userInfo();
+    const machineName = machine.username;
+    const pid = process.pid
+    return `   ${machineName.toUpperCase()} - Cart ${pid}`;
+}
+
+const formatData = ()=>{
+    var data = ''
+    var mese = ''
+    
+    var oggi = new Date();
+    let giorno = oggi.toLocaleString('en-us', {weekday: 'long'});
+    switch(oggi.getMonth()){
+        case 0: mese += 'Gen'; break;
+        case 1: mese += 'Feb'; break;
+        case 2: mese += 'Mar'; break;
+        case 3: mese += 'Apr'; break;
+        case 4: mese += 'Mag'; break;
+        case 5: mese += 'Giu'; break;
+        case 6: mese += 'Lul'; break;
+        case 7: mese += 'Ago'; break;
+        case 8: mese += 'Set'; break;
+        case 9: mese += 'Ott'; break;
+        case 10: mese += 'Nov'; break;
+        case 11: mese += 'Dic'; break;
+    }
+    return data = '\t' + giorno + '\t'+ mese + ' ' + oggi.getDate() + ' ' +oggi.getFullYear()
+    
+}
+const getPercentFromPromocode = function (promoCodeName){
+    if(promoCodeName !=='' && promoCodeName !== undefined && promoCodeName !== null){
+        let rate = promoCode.find(item => promoCodeName === item.name);
+        return rate.percentage;
+    }
+    return 0;
+}
+
+const divisorio = (div, lung)=>{
+    let result = ''
+    let cont=0
+        for(cont=0;cont<lung;cont++){
+            if (cont == 0 || cont == lung-1)
+                result += div
+            else if(cont == 1 || cont == (lung-2))
+                result += ' '
+            else
+                result += '-'
+}
+    return result;
+}
+
+const formatProductName = (product => product);
+//Modo più giusto per scrivere la funzione:
+//funzione anonima:
+/*
+const getProduct= function (productId){
+    return products.find(product => productId === product.ean)
+}*/
+
+
+
+//const formatProductList = (products => products)
+//const getProductByCart = (products => products);
+//const filterType = (products, type) => product
+//const sumCartItem = (products)=> (products)
+//const printReceipt = (contents, filename)=> '';
+//const createDeLimiter = (openClose, symbol, times)=> ''
+//const getUserDiscount = () => 0;
+//const ReceiptFileName = (uuid, date)=>'';
+
+export{
     discountedPrice,
-};
+    calculateDiscountedPrice,
+    getUser,
+    getProduct,
+    getPercentFromPromocode,
+    formatProductName,
+    divisorio,
+    printShopName,
+    formatData
+}
